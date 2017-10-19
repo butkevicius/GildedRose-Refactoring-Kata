200684935687029bdb437065618415f23be715ae
Andrius Butkevicius <andbtk@telesoftas.com>
2017-10-19 13:55:43 +0300

    Dojo #1

--- php/test/bootstrap.php
+++ php/test/bootstrap.php
@@ -1,2 +1,5 @@
 <?php
+
 set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__.'/../src');
+
+require __DIR__.'/../vendor/autoload.php';
--- php/test/gilded_rose_test.php
+++ php/test/gilded_rose_test.php
@@ -1,14 +1,43 @@
 <?php
 
+use PHPUnit\Framework\TestCase;
+
 require_once 'gilded_rose.php';
 
-class GildedRoseTest extends PHPUnit_Framework_TestCase {
+class GildedRoseTest extends TestCase
+{
 
-    function testFoo() {
-        $items = array(new Item("foo", 0, 0));
-        $gildedRose = new GildedRose($items);
+    public function test_sulfur_is_not_degrading()
+    {
+        $quality = 80;
+        $item = new Item("Sulfuras, Hand of Ragnaros", 1, $quality);
+        $gildedRose = $this->makeGildedRoseWithOneItem($item);
+
         $gildedRose->update_quality();
-        $this->assertEquals("fixme", $items[0]->name);
+
+        $this->assertSame($quality, $item->quality);
     }
+
+    public function test_aged_brie_increases_in_quality_by_1()
+    {
+        $item = new Item("Aged Brie", 1, 1);
+        $gildedRose = $this->makeGildedRoseWithOneItem($item);
+
+        $gildedRose->update_quality();
+
+        $this->assertSame(2, $item->quality);
+    }
+
+    /**
+     * @param $item
+     * @return GildedRose
+     */
+    private function makeGildedRoseWithOneItem($item): GildedRose
+    {
+        $items = array($item);
+        $gildedRose = new GildedRose($items);
+        return $gildedRose;
+    }
+
 
 }
